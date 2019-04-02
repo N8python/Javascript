d54ba7262dff16716aa9943bc5f33f8c4bf8dced
n8programs <43304488+N8python@users.noreply.github.com>
2019-04-02 16:11:08 -0400

    Create intro.js

---
+++ Javascript Tutorials/intro.js
@@ -0,0 +1,31 @@
+//Variables
+let x = 5; //Number
+let y = "Hello"; //String
+let z = true; //Boolean
+5+2+3+4; //Javascript is a calculator
+x + y = "5Hello"; //+ also does concatination
+"" + anything; //Stringification
+//Operators
+2 + 2 = 4;
+2 - 2 = 0;
+2 * 2 = 4;
+2 / 2 = 1;
+2**2 = 4; //Exponents
+2 % 2 = 0;
+//Other cool operators found in Math
+Math.floor(3.2) //3
+Math.ceil(3.2) //4
+Math.max(3, 4, 5) //5
+Math.min(3, 4, 5) //3
+//There are more, but you don't need to worry about them (for now!)
+let arr = [1, 2, 3] //Arrays are like in python, start counting at zero.
+arr[0] //1
+arr[2] //3
+arr[4] //undefined!
+//Null, NaN, Infinity, Undefined;
+let x; //x is undefined
+let x = null; //null is sorta like undefined, but slightly different.
+let x = 2 * "erer"; //NaN, when a mathmatical operation fails completely.
+let x = 10**10**100 //A googleplex is to big for JS, becomes Infinity
+//More on arrays.
+arr.length //returns 3
